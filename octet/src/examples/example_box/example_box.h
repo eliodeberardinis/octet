740d192a2223c4d4816279851beae44b15c4b91a
lios87 <eliodeb@gmail.com>
2015-09-30 15:11:25 +0100

    Playing with boxes
    Changed colour of the first box, added a second box of different colour,
    translated the second box on the x-axis and changed the speed

--- octet/src/examples/example_box/example_box.h
+++ octet/src/examples/example_box/example_box.h
@@ -19,11 +19,16 @@ namespace octet {
       app_scene =  new visual_scene();
       app_scene->create_default_camera_and_lights();
 
-      material *red = new material(vec4(1, 0, 0, 1));
+      material *green = new material(vec4(146.0f/256, 212.0f/256, 144.0f/256, 1));
+	  material *blue = new material(vec4(0, 0, 1, 1));
       mesh *box = new mesh_box(vec3(4));
       scene_node *node = new scene_node();
+	  scene_node *node2 = new scene_node();
+	  node2->translate(vec3(8, 0, 0));
       app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+	  app_scene->add_child(node2);
+      app_scene->add_mesh_instance(new mesh_instance(node, box, green));
+	  app_scene->add_mesh_instance(new mesh_instance(node2, box, blue));
     }
 
     /// this is called to draw the world
@@ -40,8 +45,12 @@ namespace octet {
 
       // tumble the box  (there is only one mesh instance)
       scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 1, 0));
-      //node->rotate(1, vec3(0, 1, 0));
+      node->rotate(9, vec3(1, 0, 0));
+	  node->rotate(9, vec3(0, 1, 0));
+      
+	  scene_node *node2 = app_scene->get_mesh_instance(1)->get_node();
+	  //node2->rotate(1, vec3(1, 0, 0));
+	  node2->rotate(0.3, vec3(0, 1, 0));
     }
   };
 }
