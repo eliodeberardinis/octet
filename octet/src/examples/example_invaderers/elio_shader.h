2bd7ced1abeda2f91511ab5a0c83567c9191ece5
lios87 <eliodeb@gmail.com>
2015-11-08 17:43:26 +0000

    make the shader change colour under certain events
    Credits: Mircea

--- octet/src/examples/example_invaderers/elio_shader.h
+++ octet/src/examples/example_invaderers/elio_shader.h
@@ -2,6 +2,7 @@ namespace octet {
 	namespace shaders {
 		class elio_shader : public shader {
 			GLuint modelToProjectionIndex_;
+			GLuint colorIndex_;
 
 		public:
 			void init() {
@@ -17,20 +18,24 @@ namespace octet {
 
 				const char fragment_shader[] = SHADER_STR(
 
+					uniform vec4 color;
+
 					void main() { 
 						vec2 p = gl_FragCoord.xy / vec2(750, 750);
 						vec3 col = vec3(0.4, 0.8, 0.1) /** p.y*/;
-						gl_FragColor = vec4(col, 1.0); 
+						gl_FragColor = color; 
 					}
 				);
 
 				shader::init(vertex_shader, fragment_shader);
 
+				colorIndex_ = glGetUniformLocation(program(), "color");
 				modelToProjectionIndex_ = glGetUniformLocation(program(), "modelToProjection");
 			}
 
-			void render(const mat4t &modelToProjection) {
+			void render(const mat4t &modelToProjection, const vec4 &color) {
 				shader::render();
+				glUniform4fv(colorIndex_, 1, color.get());
 				glUniformMatrix4fv(modelToProjectionIndex_, 1, GL_FALSE, modelToProjection.get());
 			}
 		};
