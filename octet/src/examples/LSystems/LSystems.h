f49be641c19d242545475881d8903b46865f26ec
lios87 <eliodeb@gmail.com>
2015-11-10 18:44:51 +0000

    commenting out unused stuff

--- octet/src/examples/LSystems/LSystems.h
+++ octet/src/examples/LSystems/LSystems.h
@@ -4,11 +4,19 @@
 //
 // Modular Framework for OpenGLES2 rendering on multiple platforms.
 //
+
+#include "tree_elio.h"
+
 namespace octet {
   /// Scene containing a box with octet.
+
   class LSystems : public app {
+
     // scene for drawing box
     ref<visual_scene> app_scene;
+
+	tree t;
+
   public:
     /// this is called when we construct the class before everything is initialised.
     LSystems(int argc, char **argv) : app(argc, argv) {
@@ -16,32 +24,35 @@ namespace octet {
 
     /// this is called once OpenGL is initialized
     void app_init() {
-      app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
 
+	  t.read_file("assets/lsystems/lsystem.txt");
+
+      /*app_scene =  new visual_scene();
+      app_scene->create_default_camera_and_lights();
       material *red = new material(vec4(1, 0, 0, 1));
       mesh_box *box = new mesh_box(vec3(4));
       scene_node *node = new scene_node();
       app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
+      app_scene->add_mesh_instance(new mesh_instance(node, box, red));*/
     }
 
     /// this is called to draw the world
     void draw_world(int x, int y, int w, int h) {
-      int vx = 0, vy = 0;
-      get_viewport_size(vx, vy);
-      app_scene->begin_render(vx, vy);
 
-      // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+      //int vx = 0, vy = 0;
+      //get_viewport_size(vx, vy);
+      //app_scene->begin_render(vx, vy);
 
-      // draw the scene
-      app_scene->render((float)vx / vy);
+      //// update matrices. assume 30 fps.
+      //app_scene->update(1.0f/30);
 
-      // tumble the box  (there is only one mesh instance)
-      scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
+      //// draw the scene
+      //app_scene->render((float)vx / vy);
+
+      //// tumble the box  (there is only one mesh instance)
+      //scene_node *node = app_scene->get_mesh_instance(0)->get_node();
+      //node->rotate(1, vec3(1, 0, 0));
+      //node->rotate(1, vec3(0, 1, 0));
     }
   };
 }
