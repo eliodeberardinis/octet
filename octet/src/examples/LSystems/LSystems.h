80917d4ce488251bcd41e8853b7ac8e893363463
lios87 <eliodeb@gmail.com>
2015-12-06 19:43:03 +0000

    small change

--- octet/src/examples/LSystems/LSystems.h
+++ octet/src/examples/LSystems/LSystems.h
@@ -142,7 +142,8 @@ namespace octet {
 			std::cout << "- f3: Change season (Summer, Fall, Spring, Winter)\n";
 			std::cout << "- delete: Rotate the model\n";
 			std::cout << "- Shift/Ctrl: Zoom in/Zoom out\n";
-			std::cout << "- Arrow keys: Move camera";
+			std::cout << "- Arrow keys: Move camera\n";
+			std::cout << "- Mouse Left/Right Click: Change Example";
 			std::cout << "\n\nCurrent example: "<<current_example<<"\n";
 			std::cout << "Season: Summer\n";
 
